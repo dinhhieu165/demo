130d6b55e3013e1fdaa26f0f329734452cbfd57d
Hieu Dinh <dinhhieu16598@gmail.com>
2019-01-29 10:45:53 +0700

    first

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "Demo_Deploy"
+set :repo_url, "git@github.com:dinhhieu165/demo.git"
+
+set :branch, :master
+set :deploy_to, '/home/ubuntu/demo'
+set :pty, true
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 5
+set :rvm_type, :user
+set :rvm_ruby_version, '2.5.1'
+set :ssh_options, { forward_agent: true } # Edit this if you are using MRI Ruby
+set :tmp_dir, "/home/ubuntu/demo/tmp"
+set :rvm_map_bins, %w{gem rake ruby rails bundle}
+set :passenger_restart_with_touch, true
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, false
